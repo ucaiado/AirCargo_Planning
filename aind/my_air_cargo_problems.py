9e9ab274a7af2730acb50d14d7b487eeb5db4a1f
ucaiado <ucaiado@me.com>
2017-09-07 22:53:12 -0300

    feat: finish code related to the problem specification

--- aind/my_air_cargo_problems.py
+++ aind/my_air_cargo_problems.py
@@ -53,7 +53,7 @@ class AirCargoProblem(Problem):
         :return: list<Action>. list of Action objects
         """
 
-        # TODO create concrete Action objects based on the domain action
+        # Create concrete Action objects based on the domain action
         # schema for: Load, Unload, and Fly
         # concrete actions definition: specific literal action that does not
         # include variables as with the schema. For example, the action schema
@@ -68,7 +68,24 @@ class AirCargoProblem(Problem):
             :return: list of Action objects
             """
             loads = []
-            # TODO create all load ground actions from the domain Load action
+            # create all load ground actions from the domain Load action
+            # the type should not be tested. Is is already assumed
+            # Action(Load(c, p, a),
+            #   PRECOND: At(c, a) ∧ At(p, a) ∧ Cargo(c) ∧ Plane(p) ∧ Airport(a)
+            #   EFFECT: ¬ At(c, a) ∧ In(c, p))
+            for a in self.airports:
+                for c in self.cargos:
+                    for p in self.planes:
+                        precond_pos = [expr('At({}, {})'.format(c, a)),
+                                       expr('At({}, {})'.format(p, a))]
+                        precond_neg = []
+                        s_txt = 'Load({}, {}, {})'
+                        effect_add = [expr('In({}, {})'.format(c, p))]
+                        effect_rem = [expr('At({}, {})'.format(c, a))]
+                        load = Action(expr(s_txt.format(p, a, a)),
+                                      [precond_pos, precond_neg],
+                                      [effect_add, effect_rem])
+                        loads.append(load)
             return loads
 
         def unload_actions():
@@ -77,7 +94,24 @@ class AirCargoProblem(Problem):
             :return: list of Action objects
             """
             unloads = []
-            # TODO create all Unload ground actions from the domain Unload actn
+            # create all Unload ground actions from the domain Unload actn
+            # the type should not be tested. Is is already assumed
+            # Action(Unload(c, p, a),
+            #   PRECOND: In(c, p) ∧ At(p, a) ∧ Cargo(c) ∧ Plane(p) ∧ Airport(a)
+            #   EFFECT: At(c, a) ∧ ¬ In(c, p))
+            for a in self.airports:
+                for c in self.cargos:
+                    for p in self.planes:
+                        precond_pos = [expr('In({}, {})'.format(c, p)),
+                                       expr('At({}, {})'.format(p, a))]
+                        precond_neg = []
+                        s_txt = 'Unload({}, {}, {})'
+                        effect_add = [expr('At({}, {})'.format(c, a))]
+                        effect_rem = [expr('In({}, {})'.format(c, p))]
+                        unload = Action(expr(s_txt.format(p, a, a)),
+                                        [precond_pos, precond_neg],
+                                        [effect_add, effect_rem])
+                        unloads.append(unload)
             return unloads
 
         def fly_actions():
@@ -90,12 +124,12 @@ class AirCargoProblem(Problem):
                 for to in self.airports:
                     if fr != to:
                         for p in self.planes:
-                            precond_pos = [expr("At({}, {})".format(p, fr)),
+                            precond_pos = [expr('At({}, {})'.format(p, fr)),
                                            ]
                             precond_neg = []
-                            s_txt = "Fly({}, {}, {})"
-                            effect_add = [expr("At({}, {})".format(p, to))]
-                            effect_rem = [expr("At({}, {})".format(p, fr))]
+                            s_txt = 'Fly({}, {}, {})'
+                            effect_add = [expr('At({}, {})'.format(p, to))]
+                            effect_rem = [expr('At({}, {})'.format(p, fr))]
                             fly = Action(expr(s_txt.format(p, fr, to)),
                                          [precond_pos, precond_neg],
                                          [effect_add, effect_rem])
@@ -112,8 +146,19 @@ class AirCargoProblem(Problem):
             e.g. 'FTTTFF'
         :return: list of Action objects
         """
-        # TODO implement
         possible_actions = []
+        kb = PropKB()
+        kb.tell(decode_state(state, self.state_map).pos_sentence())
+        for action in self.actions_list:
+            is_possible = True
+            for clause in action.precond_pos:
+                if clause not in kb.clauses:
+                    is_possible = False
+            for clause in action.precond_neg:
+                if clause in kb.clauses:
+                    is_possible = False
+            if is_possible:
+                possible_actions.append(action)
         return possible_actions
 
     def result(self, state: str, action: Action):
@@ -125,8 +170,20 @@ class AirCargoProblem(Problem):
         :param action: Action applied
         :return: resulting state after action
         """
-        # TODO implement
         new_state = FluentState([], [])
+        old_state = decode_state(state, self.state_map)
+        for fluent in old_state.pos:
+            if fluent not in action.effect_rem:
+                new_state.pos.append(fluent)
+        for fluent in action.effect_add:
+            if fluent not in new_state.pos:
+                new_state.pos.append(fluent)
+        for fluent in old_state.neg:
+            if fluent not in action.effect_add:
+                new_state.neg.append(fluent)
+        for fluent in action.effect_rem:
+            if fluent not in new_state.neg:
+                new_state.neg.append(fluent)
         return encode_state(new_state, self.state_map)
 
     def goal_test(self, state: str) -> bool:
@@ -166,12 +223,23 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3)
+        # implement (see Russell-Norvig Ed-3 10.2.3)
+        # count the number of unsatisfied goals
         count = 0
+        node_state = decode_state(node.state, self.state_map).pos
+        count = len([goal for goal in self.goal if goal not in node_state])
         return count
 
 
 def air_cargo_p1() -> AirCargoProblem:
+    '''implement Problem 2 definition:
+    Init(At(C1, SFO) ∧ At(C2, JFK)
+        ∧ At(P1, SFO) ∧ At(P2, JFK)
+        ∧ Cargo(C1) ∧ Cargo(C2)
+        ∧ Plane(P1) ∧ Plane(P2)
+        ∧ Airport(JFK) ∧ Airport(SFO))
+    Goal(At(C1, JFK) ∧ At(C2, SFO))
+    '''
     cargos = ['C1', 'C2']
     planes = ['P1', 'P2']
     airports = ['JFK', 'SFO']
@@ -197,10 +265,110 @@ def air_cargo_p1() -> AirCargoProblem:
 
 
 def air_cargo_p2() -> AirCargoProblem:
-    # TODO implement Problem 2 definition
-    pass
+    '''implement Problem 2 definition:
+    Init(At(C1, SFO) ∧ At(C2, JFK) ∧ At(C3, ATL)
+        ∧ At(P1, SFO) ∧ At(P2, JFK) ∧ At(P3, ATL)
+        ∧ Cargo(C1) ∧ Cargo(C2) ∧ Cargo(C3)
+        ∧ Plane(P1) ∧ Plane(P2) ∧ Plane(P3)
+        ∧ Airport(JFK) ∧ Airport(SFO) ∧ Airport(ATL))
+    Goal(At(C1, JFK) ∧ At(C2, SFO) ∧ At(C3, SFO))
+    '''
+    cargos = ['C1', 'C2', 'C3']
+    planes = ['P1', 'P2', 'P3']
+    airports = ['JFK', 'SFO', 'ATL']
+    pos = [expr('At(C1, SFO)'),
+           expr('At(C2, JFK)'),
+           expr('At(C3, ATL)'),
+           expr('At(P1, SFO)'),
+           expr('At(P2, JFK)'),
+           expr('At(P3, ATL)')
+           ]
+    neg = [expr('At(C1, JFK)'),
+           expr('At(C1, ATL)'),
+           expr('At(C2, SFO)'),
+           expr('At(C2, ATL)'),
+           expr('At(C3, SFO)'),
+           expr('At(C3, JFK)'),
+           expr('In(C1, P1)'),
+           expr('In(C1, P2)'),
+           expr('In(C1, P3)'),
+           expr('In(C2, P1)'),
+           expr('In(C2, P2)'),
+           expr('In(C2, P3)'),
+           expr('In(C3, P1)'),
+           expr('In(C3, P2)'),
+           expr('In(C3, P3)'),
+           expr('At(P1, JFK)'),
+           expr('At(P1, ATL)'),
+           expr('At(P2, SFO)'),
+           expr('At(P2, ATL)'),
+           expr('At(P3, SFO)'),
+           expr('At(P3, JFK)')]
+    init = FluentState(pos, neg)
+    goal = [expr('At(C1, JFK)'),
+            expr('At(C2, SFO)'),
+            expr('At(C3, SFO)')
+            ]
+
+    return AirCargoProblem(cargos, planes, airports, init, goal)
 
 
 def air_cargo_p3() -> AirCargoProblem:
-    # TODO implement Problem 3 definition
-    pass
+    '''implement Problem 3 definition
+    Init(At(C1, SFO) ∧ At(C2, JFK) ∧ At(C3, ATL) ∧ At(C4, ORD)
+        ∧ At(P1, SFO) ∧ At(P2, JFK)
+        ∧ Cargo(C1) ∧ Cargo(C2) ∧ Cargo(C3) ∧ Cargo(C4)
+        ∧ Plane(P1) ∧ Plane(P2)
+        ∧ Airport(JFK) ∧ Airport(SFO) ∧ Airport(ATL) ∧ Airport(ORD))
+    Goal(At(C1, JFK) ∧ At(C3, JFK) ∧ At(C2, SFO) ∧ At(C4, SFO))
+    '''
+    cargos = ['C1', 'C2', 'C3', 'C4']
+    planes = ['P1', 'P2']
+    airports = ['JFK', 'SFO', 'ATL', 'ORD']
+    pos = [expr('At(C1, SFO)'),
+           expr('At(C2, JFK)'),
+           expr('At(C3, ATL)'),
+           expr('At(C4, ORD)'),
+           expr('At(P1, SFO)'),
+           expr('At(P2, JFK)')
+           ]
+    neg = [expr('At(C1, JFK)'),
+           expr('At(C1, ATL)'),
+           expr('At(C1, ORD)'),
+
+           expr('At(C2, SFO)'),
+           expr('At(C2, ATL)'),
+           expr('At(C2, ORD)'),
+
+           expr('At(C3, SFO)'),
+           expr('At(C3, JFK)'),
+           expr('At(C3, ORD)'),
+
+           expr('At(C4, SFO)'),
+           expr('At(C4, JFK)'),
+           expr('At(C4, ATL)'),
+
+           expr('In(C1, P1)'),
+           expr('In(C1, P2)'),
+           expr('In(C2, P1)'),
+           expr('In(C2, P2)'),
+           expr('In(C3, P1)'),
+           expr('In(C3, P2)'),
+           expr('In(C4, P1)'),
+           expr('In(C4, P2)'),
+
+           expr('At(P1, JFK)'),
+           expr('At(P1, ATL)'),
+           expr('At(P1, ORD)'),
+
+           expr('At(P2, SFO)'),
+           expr('At(P2, ATL)'),
+           expr('At(P2, ORD)')]
+    init = FluentState(pos, neg)
+    goal = [expr('At(C1, JFK)'),
+            expr('At(C2, SFO)'),
+            expr('At(C3, JFK)'),
+            expr('At(C4, SFO)')
+            ]
+
+    return AirCargoProblem(cargos, planes, airports, init, goal)
--- aind/run_search.py
+++ aind/run_search.py
@@ -2,9 +2,10 @@ import argparse
 from timeit import default_timer as timer
 from aimacode.search import InstrumentedProblem
 from aimacode.search import (breadth_first_search, astar_search,
-    breadth_first_tree_search, depth_first_graph_search, uniform_cost_search,
-    greedy_best_first_graph_search, depth_limited_search,
-    recursive_best_first_search)
+                             breadth_first_tree_search, depth_limited_search,
+                             depth_first_graph_search, uniform_cost_search,
+                             greedy_best_first_graph_search,
+                             recursive_best_first_search)
 from my_air_cargo_problems import air_cargo_p1, air_cargo_p2, air_cargo_p3
 
 PROBLEM_CHOICE_MSG = """
@@ -23,16 +24,18 @@ You must either use the -m flag to run in manual mode, or use both the -p and
 choices for each include:
 """
 
-PROBLEMS = [["Air Cargo Problem 1", air_cargo_p1],
-            ["Air Cargo Problem 2", air_cargo_p2],
-            ["Air Cargo Problem 3", air_cargo_p3]]
-SEARCHES = [["breadth_first_search", breadth_first_search, ""],
-            ['breadth_first_tree_search', breadth_first_tree_search, ""],
-            ['depth_first_graph_search', depth_first_graph_search, ""],
-            ['depth_limited_search', depth_limited_search, ""],
-            ['uniform_cost_search', uniform_cost_search, ""],
-            ['recursive_best_first_search', recursive_best_first_search, 'h_1'],
-            ['greedy_best_first_graph_search', greedy_best_first_graph_search, 'h_1'],
+PROBLEMS = [['Air Cargo Problem 1', air_cargo_p1],
+            ['Air Cargo Problem 2', air_cargo_p2],
+            ['Air Cargo Problem 3', air_cargo_p3]]
+SEARCHES = [['breadth_first_search', breadth_first_search, ''],
+            ['breadth_first_tree_search', breadth_first_tree_search, ''],
+            ['depth_first_graph_search', depth_first_graph_search, ''],
+            ['depth_limited_search', depth_limited_search, ''],
+            ['uniform_cost_search', uniform_cost_search, ''],
+            ['recursive_best_first_search', recursive_best_first_search,
+             'h_1'],
+            ['greedy_best_first_graph_search', greedy_best_first_graph_search,
+             'h_1'],
             ['astar_search', astar_search, 'h_1'],
             ['astar_search', astar_search, 'h_ignore_preconditions'],
             ['astar_search', astar_search, 'h_pg_levelsum'],
@@ -46,7 +49,8 @@ class PrintableProblem(InstrumentedProblem):
     """
 
     def __repr__(self):
-        return '{:^10d}  {:^10d}  {:^10d}'.format(self.succs, self.goal_tests, self.states)
+        s_txt = '{:^10d}  {:^10d}  {:^10d}'
+        return s_txt.format(self.succs, self.goal_tests, self.states)
 
 
 def run_search(problem, search_function, parameter=None):
@@ -58,8 +62,8 @@ def run_search(problem, search_function, parameter=None):
     else:
         node = search_function(ip)
     end = timer()
-    print("\nExpansions   Goal Tests   New Nodes")
-    print("{}\n".format(ip))
+    print('\nExpansions   Goal Tests   New Nodes')
+    print('{}\n'.format(ip))
     show_solution(node, end - start)
     print()
 
@@ -68,21 +72,20 @@ def manual():
 
     print(PROBLEM_CHOICE_MSG)
     for idx, (name, _) in enumerate(PROBLEMS):
-        print("    {!s}. {}".format(idx+1, name))
-    p_choices = input("> ").split()
+        print('    {!s}. {}'.format(idx+1, name))
+    p_choices = input('> ').split()
 
     print(SEARCH_METHOD_CHOICE_MSG)
     for idx, (name, _, heuristic) in enumerate(SEARCHES):
-        print("    {!s}. {} {}".format(idx+1, name, heuristic))
-    s_choices = input("> ").split()
+        print('    {!s}. {} {}'.format(idx+1, name, heuristic))
+    s_choices = input('> ').split()
 
     main(p_choices, s_choices)
-
-    print("\nYou can run this selection again automatically from the command " +
-          "line\nwith the following command:")
-    print("\n  python {} -p {} -s {}\n".format(__file__,
-                                               " ".join(p_choices),
-                                               " ".join(s_choices)))
+    s = '\nYou can run this selection again automatically from the command '
+    print(s + 'line\nwith the following command:')
+    print('\n  python {} -p {} -s {}\n'.format(__file__,
+                                               ' '.join(p_choices),
+                                               ' '.join(s_choices)))
 
 
 def main(p_choices, s_choices):
@@ -93,8 +96,8 @@ def main(p_choices, s_choices):
     for pname, p in problems:
 
         for sname, s, h in searches:
-            hstring = h if not h else " with {}".format(h)
-            print("\nSolving {} using {}{}...".format(pname, sname, hstring))
+            hstring = h if not h else ' with {}'.format(h)
+            print('\nSolving {} using {}{}...'.format(pname, sname, hstring))
 
             _p = p()
             _h = None if not h else getattr(_p, h)
@@ -102,36 +105,47 @@ def main(p_choices, s_choices):
 
 
 def show_solution(node, elapsed_time):
-    print("Plan length: {}  Time elapsed in seconds: {}".format(len(node.solution()), elapsed_time))
+    s_txt = 'Plan length: {}  Time elapsed in seconds: {}'
+    print(s_txt.format(len(node.solution()), elapsed_time))
     for action in node.solution():
-        print("{}{}".format(action.name, action.args))
+        print('{}{}'.format(action.name, action.args))
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(description="Solve air cargo planning problems " + 
-        "using a variety of state space search methods including uninformed, greedy, " +
-        "and informed heuristic search.")
-    parser.add_argument('-m', '--manual', action="store_true",
-                        help="Interactively select the problems and searches to run.")
-    parser.add_argument('-p', '--problems', nargs="+", choices=range(1, len(PROBLEMS)+1), type=int, metavar='',
-                        help="Specify the indices of the problems to solve as a list of space separated values. Choose from: {!s}".format(list(range(1, len(PROBLEMS)+1))))
-    parser.add_argument('-s', '--searches', nargs="+", choices=range(1, len(SEARCHES)+1), type=int, metavar='',
-                        help="Specify the indices of the search algorithms to use as a list of space separated values. Choose from: {!s}".format(list(range(1, len(SEARCHES)+1))))
+if __name__ == '__main__':
+    s_txt = 'Solve air cargo planning problems '
+    s_txt += 'using a variety of state space search methods including '
+    s_txt += 'uninformed, greedy, and informed heuristic search.'
+    parser = argparse.ArgumentParser(description=s_txt)
+    s_txt = 'Interactively select the problems and searches to run.'
+    parser.add_argument('-m', '--manual', action='store_true', help=s_txt)
+    s_txt = 'Specify the indices of the problems to solve as a list of space '
+    s_txt += 'separated values. Choose from: {!s}'
+    parser.add_argument('-p', '--problems', nargs='+', metavar='',
+                        choices=range(1, len(PROBLEMS)+1), type=int,
+                        help=s_txt.format(list(range(1, len(PROBLEMS)+1))))
+    s_txt = 'Specify the indices of the search algorithms to use as a list of '
+    s_txt += 'space separated values. Choose from: {!s}'
+    parser.add_argument('-s', '--searches', nargs='+', type=int, metavar='',
+                        choices=range(1, len(SEARCHES)+1),
+                        help=s_txt.format(list(range(1, len(SEARCHES)+1))))
     args = parser.parse_args()
 
     if args.manual:
         manual()
     elif args.problems and args.searches:
-        main(list(sorted(set(args.problems))), list(sorted(set((args.searches)))))
+        main(list(sorted(set(args.problems))),
+             list(sorted(set((args.searches)))))
     else:
         print()
         parser.print_help()
         print(INVALID_ARG_MSG)
-        print("Problems\n-----------------")
+        print('Problems\n-----------------')
         for idx, (name, _) in enumerate(PROBLEMS):
-            print("    {!s}. {}".format(idx+1, name))
+            print('    {!s}. {}'.format(idx+1, name))
         print()
-        print("Search Algorithms\n-----------------")
+        print('Search Algorithms\n-----------------')
         for idx, (name, _, heuristic) in enumerate(SEARCHES):
-            print("    {!s}. {} {}".format(idx+1, name, heuristic))
+            print('    {!s}. {} {}'.format(idx+1, name, heuristic))
         print()
-        print("Use manual mode for interactive selection:\n\n\tpython run_search.py -m\n")
+        s_txt = 'Use manual mode for interactive selection:\n\n\tpython'
+        s_txt += 'run_search.py -m\n'
+        print(s_txt)
